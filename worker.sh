d19c7ab5e65c1b183fe50c8e3be479f6bc7e09e3
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2022-11-27 18:59:27 -0300

    first Initial

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-1x4idkivii2675aturde2skr85x0hz9v3pcj69gano7w1zibb8-24hnlohpct78y8glfk5y267er 10.10.10.100:2377
